775e5791535797b730bde700f7ee12da26f57609
Loric <loricjousson@gmail.com>
2026-04-03 16:48:36 +0200

    Fix React Compiler runtime resolution

--- tasks/reactCompilerPlugin.ts
+++ tasks/reactCompilerPlugin.ts
@@ -1,17 +1,34 @@
-import babel from "esbuild-plugin-babel";
+import babel from "@babel/core";
+import type { Plugin } from "@esbuild/mod.js";
 
-export const reactCompilerPlugin = () => {
-  return babel({
-    config: {
-      presets: ["@babel/preset-typescript", ["@babel/preset-react", { runtime: "automatic" }]],
-      plugins: [
-        [
-          "babel-plugin-react-compiler",
-          {
-            target: "18",
-          },
-        ],
-      ],
+export const reactCompilerPlugin = (): Plugin => {
+  return {
+    name: "react-compiler",
+    setup(build) {
+      build.onLoad({ filter: /\.(ts|tsx|js|jsx)$/ }, async (args) => {
+        const code = await Deno.readTextFile(args.path);
+        
+        const result = babel.transformSync(code, {
+          filename: args.path,
+          presets: [
+            "@babel/preset-typescript",
+            ["@babel/preset-react", { runtime: "automatic" }],
+          ],
+          plugins: [
+            ["babel-plugin-react-compiler", { target: "18" }],
+          ],
+          sourceType: "module",
+          ast: false,
+          code: true,
+          configFile: false,
+          babelrc: false,
+        });
+
+        return {
+          contents: result?.code || code,
+          loader: "jsx",
+        };
+      });
     },
-  });
+  };
 };
